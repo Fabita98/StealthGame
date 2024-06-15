4c6e960a407ddc81e48816be1c800b04c9974583
Fabita98 <campo.fabrizio9@gmail.com>
2024-06-15 17:41:35 +0200

    GazeTracking setup
    - EyeInteractable layer and objects;
    - Rays to check where player is looking;
    To be tested with headset.

---
+++ Assets/Scripts/Player/GazeTrackingFeature/EyeInteractable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.GazeTrackingFeature
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
+    internal class EyeInteractable : MonoBehaviour
+    {
+        public bool IsHovered { get; set; }
+        [SerializeField] private UnityEvent<GameObject> OnObjectHover;
+        [SerializeField] private Material OnHoverActiveMaterial; 
+        [SerializeField] private Material OnHoverInactiveMaterial; 
+        private MeshRenderer meshRenderer;
+
+        void Start() => meshRenderer = GetComponent<MeshRenderer>();
+
+        private void Update()
+        {
+            if (IsHovered) { 
+                meshRenderer.material = OnHoverActiveMaterial;
+                OnObjectHover?.Invoke(gameObject);
+            }
+            else
+            {
+                meshRenderer.material = OnHoverInactiveMaterial;
+            }
+        }
+    }
+}
