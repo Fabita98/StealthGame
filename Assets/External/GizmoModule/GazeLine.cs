efc0f3750a632a1ade4626bdc8c635a5a66a706f
Fabita98 <campo.fabrizio9@gmail.com>
2024-06-18 18:52:06 +0200

    GazeTracking correctly working
    GazeLine script changed to work as TracingRay to highlight objects we are looking at.

--- Assets/External/GizmoModule/GazeLine.cs
+++ Assets/External/GizmoModule/GazeLine.cs
@@ -1,31 +1,47 @@
-using System.Collections;
+using Assets.Scripts.GazeTrackingFeature;
 using System.Collections.Generic;
 using Unity.Labs.SuperScience;
 using UnityEngine;
 
 public class GazeLine : MonoBehaviour
 {
-
     public float cursorOffset, cursorRadius;
 
     public LayerMask mask;
 
     private Vector3 hitPosition;
+    private List<EyeInteractable> eyeInteractables = new();
 
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(origin: transform.position, direction: transform.forward, out hit, Mathf.Infinity, layerMask: mask))
-        {
+        if (Physics.Raycast(origin: transform.position, direction: transform.forward, out RaycastHit hit, Mathf.Infinity, layerMask: mask)) {
             hitPosition = hit.point;
             cursorRadius = .5f;
-        }      
+            UnSelect();
+
+            if (hit.collider.TryGetComponent<EyeInteractable>(out var eyeInteractable))
+            {
+                eyeInteractables.Add(eyeInteractable);
+                eyeInteractable.IsHovered = true;
+            }
+        }
+        else
+        {
+            UnSelect(true);
+        }
     }
 
     private void Update()
     {
         GizmoModule.instance.DrawSphere(hitPosition + (transform.position - hitPosition).normalized * cursorOffset, cursorRadius, Color.red);
+    }
+
+    void UnSelect(bool clear = false)
+    {
+        foreach (EyeInteractable interactable in eyeInteractables)
+        {
+            interactable.IsHovered = false;
+        }
+        if (clear) eyeInteractables.Clear();
     }
 }
