34b046bd29f5dc70352110789027f1f00e9eb20a
massimilianosentineri <max.sentineri@gmail.com>
2024-12-01 22:21:10 +0100

    Main level updates (lights to fix)
    Bridge animation, game lights, level design, shadows, map colliders

--- Assets/move_bridge.cs
+++ Assets/move_bridge.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class open_door : MonoBehaviour    
+public class move_bridge : MonoBehaviour
 {
+
     public Animator an;
-    public AudioSource DoorSound;
-    bool play = false;
+    public AudioSource BridgeSound;
+    bool play, isLowered = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DoorSound.isPlaying == false && play)  Invoke("playsound", 0.4f); 
+        if (BridgeSound.isPlaying == false && play && !isLowered) Invoke("playsound", 0.2f);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag=="LeftHand"|| other.tag == "RightHand")
+        if (other.tag == "LeftHand" || other.tag == "RightHand")
         {
             play = true;
-            an.SetTrigger("open");
+            an.SetBool("isLowered", true);
 
         }
     }
@@ -33,6 +34,7 @@ public class open_door : MonoBehaviour
     }
     void playsound()
     {
-        DoorSound.Play(); play = false;
+        BridgeSound.Play();
+        isLowered = true;
     }
 }
